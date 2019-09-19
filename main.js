3364185bc6cdb6f307bfe331f26c0c7cb78c9820
Matt Gouchoe-Hanas <mattgh21@gmail.com>
2019-09-19 12:23:17 -0400

    adds ability to have multiple numbers in a string and evaluate operations

--- main.js
+++ main.js
@@ -3,61 +3,61 @@ console.log(field)
 const sevenButton = document.querySelector('#seven-button')
 console.log({ sevenButton })
 sevenButton.addEventListener('click', function() {
-    field.innerText = "7 "
+    field.innerText += "7"
 })
 
 const eightButton = document.querySelector('#eight-button')
 console.log({ eightButton })
 eightButton.addEventListener('click', function() {
-    field.innerText = "8"
+    field.innerText += "8"
 })
 
 const nineButton = document.querySelector('#nine-button')
 console.log({ nineButton })
 nineButton.addEventListener('click', function() {
-    field.innerText = "9"
+    field.innerText += "9"
 })
 
 const zeroButton = document.querySelector('#zero-button')
 console.log({ zeroButton })
 zeroButton.addEventListener('click', function() {
-    field.innerText = "0"
+    field.innerText += "0"
 })
 
 const oneButton = document.querySelector('#one-button')
 console.log({ oneButton })
 oneButton.addEventListener('click', function() {
-    field.innerText = "1"
+    field.innerText += "1"
 })
 
 const twoButton = document.querySelector('#two-button')
 console.log({ twoButton })
 twoButton.addEventListener('click', function() {
-    field.innerText = "2"
+    field.innerText += "2"
 })
 
 const threeButton = document.querySelector('#three-button')
 console.log({ threeButton })
 threeButton.addEventListener('click', function() {
-    field.innerText = "3"
+    field.innerText += "3"
 })
 
 const fourButton = document.querySelector('#four-button')
 console.log({ fourButton })
 fourButton.addEventListener('click', function() {
-    field.innerText = "4"
+    field.innerText += "4"
 })
 
 const fiveButton = document.querySelector('#five-button')
 console.log({ eightButton })
 fiveButton.addEventListener('click', function() {
-    field.innerText = "5"
+    field.innerText += "5"
 })
 
 const sixButton = document.querySelector('#six-button')
 console.log({ sixButton })
 sixButton.addEventListener('click', function() {
-    field.innerText = "6"
+    field.innerText += "6"
 })
 
 const clearButton = document.querySelector('#clear-button')
@@ -69,35 +69,58 @@ clearButton.addEventListener('click', function() {
 const additionButton = document.querySelector('#addition-button')
 console.log({ additionButton })
 additionButton.addEventListener('click', function() {
-    field.innerText = "+"
+    field.innerText += "+"
 })
 
 const subtractButton = document.querySelector('#subtract-button')
 console.log({ subtractButton })
 subtractButton.addEventListener('click', function() {
-    field.innerText = "-"
+    field.innerText += "-"
 })
 
 const divideButton = document.querySelector('#divide-button')
 console.log({ divideButton })
 divideButton.addEventListener('click', function() {
-    field.innerText = "/"
+    field.innerText += "/"
 })
 
 const multiplyButton = document.querySelector('#multiply-button')
 console.log({ multiplyButton })
 multiplyButton.addEventListener('click', function() {
-    field.innerText = "*"
+    field.innerText += "*"
 })
 
 const decimalButton = document.querySelector('#decimal-button')
 console.log({ decimalButton })
 decimalButton.addEventListener('click', function() {
-    field.innerText = "."
+    field.innerText += "."
 })
+
+
+
+
+
 
 const equalButton = document.querySelector('#equal-button')
 console.log({ equalButton })
-    // eightButton.addEventListener('click', function() {
-    //     field.innerText = eval(string)
-    // })
+equalButton.addEventListener('click', function(event) {
+    field.innerText = eval(field.innerText)
+})
+
+
+
+// for (let symbol of numbersAndOperators) {
+//     symbol.addEventListener('click', function() {
+//         screen.innerText += symbol.innerText;
+//         math +=symbol.innerText;
+//     })
+// }
+
+// numbersAndOperators is a reference to a class for all numbers and operators except clear and =
+
+// equalButton.addEventListener('click', function(event) {
+//     let answer = eval(disMath; display.textcontent = answer;)
+// })
+
+
+// display = field?
